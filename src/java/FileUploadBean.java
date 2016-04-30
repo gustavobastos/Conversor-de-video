f2ab5d794b82a1d1e2bdca88188ef7eed4a882d0
gustavobastos <gustavosoad@gmail.com>
2016-04-30 08:34:50 -0300

    Implementação do sistema de upload no Amazon s3

--- src/java/FileUploadBean.java
+++ src/java/FileUploadBean.java
@@ -1,9 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import java.io.IOException;
@@ -45,8 +56,13 @@ public class FileUploadBean {
 
         if (uploadedFile != null) {
             try {
+                AWSCredentials credentials = new BasicAWSCredentials("AKIAIV52M4ZQGLMOKBIA", "Oh6FuivQUm33MCJzWeR8nzaTLDjlMzUgh5xljiIL");
+                AmazonS3 s3client = new AmazonS3Client(credentials);
+                
                 InputStream is = uploadedFile.getInputStream();
-                text = new Scanner(is).useDelimiter("\\A").next();
+                ObjectMetadata obj = new ObjectMetadata();
+                obj.setContentLength(uploadedFile.getSize());
+                s3client.putObject(new PutObjectRequest("conversorfiles", "video.dv", uploadedFile.getInputStream(), obj).withCannedAcl(CannedAccessControlList.PublicRead));
             } catch (IOException ex) {
             }
         }
